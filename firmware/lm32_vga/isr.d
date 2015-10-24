638deaa242293786abeeea21aa8db7c728567aee
Rohit Kumar Singh <rohit91.2008@gmail.com>
2015-10-25 02:55:26 +0530

    Removed temporary and build o/p files

--- firmware/lm32_vga/isr.d
+++
@@ -1,9 +0,0 @@
-isr.o: isr.c ../../build/misoc/software/include/generated/csr.h \
- ../../build/misoc/software/include/hw/common.h \
- ../../build/misoc/software/include/base/irq.h \
- ../../build/misoc/software/include/base/uart.h
-isr.c:
-../../build/misoc/software/include/generated/csr.h:
-../../build/misoc/software/include/hw/common.h:
-../../build/misoc/software/include/base/irq.h:
-../../build/misoc/software/include/base/uart.h:
